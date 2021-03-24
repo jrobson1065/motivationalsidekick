0bcf155cffff2afabe88d44982408c0887bde3f9
jrobson1065 <j.robson1065@gmail.com>
2021-03-24 00:04:01 -0400

    small fixes

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ from selenium import webdriver
 import time
 import base64
 import schedule
-from gmail_service import main
+from gmail_service import create_service
 
-pw = base64.b64decode("YnJpdG5leTIy").decode("utf-8")
+pw = base64.b64decode("YnJpdG5leTIy")
 email = "j.robson1065@gmail.com"
 
 def get_quote():
     url = "https://www.oberlo.com/blog/motivational-quotes"
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
 
     quotes = soup.select(".single-post ol li span")
     return random.choice(quotes).text
@@ -30,7 +30,7 @@ def post():
     password = browser.find_element_by_name("pass")
     submit = browser.find_element_by_name("login")
     username.send_keys(email)
-    password.send_keys(pw)
+    password.send_keys(pw.decode("utf-8"))
     submit.click()
     submit = browser.find_element_by_xpath('//a[@target="_self"]')
     submit.click()
@@ -43,15 +43,14 @@ def post():
     
 def create_message(sender, to, subject, message_text):
     message = MIMEText(message_text)
-    message['to'] = to
-    message['from'] = sender
-    message['subject'] = subject
-    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
+    return {"raw": base64.urlsafe_b64encode(message.as_string().encode()).decode()}
     
 def send_message(service, user_id, message):
         message = (service.users().messages().send(userId=user_id, body=message)
                 .execute())
-        print('Message Id: %s' % message['id'])
         return message
 
 quote = get_quote()
@@ -59,7 +58,7 @@ quote = get_quote()
 def run():
     post()
     email_msg = create_message(email, email, "Daily Motivational Quote", quote)
-    service = main()
+    service = create_service()
     send_message(service, "me", email_msg)
 
 schedule.every().day.at("9:00").do(run)
--- gmail_service.py
+++ gmail_service.py
@@ -7,20 +7,20 @@ from google.oauth2.credentials import Credentials
 
 scopes = ["https://www.googleapis.com/auth/gmail.compose"]
 
-def main():
+def create_service():
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", scopes)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', scopes)
+                "credentials.json", scopes)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
+        with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    service = build('gmail', 'v1', credentials=creds)
+    service = build("gmail", "v1", credentials=creds)
 
     return service
